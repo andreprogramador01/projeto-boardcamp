869aa69c2ea67bb047576e6a2b3d945b878bf0f5
andreprogramador01 <andreprogramador01@gmail.com>
2023-02-10 13:35:25 -0300

    fix:ajuste na validação do cpf e do telefon

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -4,8 +4,8 @@ import JoiImport from 'joi';
 const joi = JoiImport.extend(DateExtension)
 
 const customerSchema = joi.object({
-cpf: joi.string().required().length(11),
- phone: joi.string().required().min(10).max(11),
+cpf: joi.string().required().length(11).pattern(/^[0-9]+$/),
+ phone: joi.string().required().min(10).max(11).pattern(/^[0-9]+$/),
  name: joi.string().required(),
  birthday: joi.date().format('YYYY-MM-DD').required()
 })
